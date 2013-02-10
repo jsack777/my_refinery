9f960e0c14786dc4a17dfa11511aff9fbebbc2ff
Jeff Sackett <Jeff.Sackett@Pearson.com>
2013-02-10 12:25:39 -0600

    force ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :redirect_https
+
+  def redirect_https
+    return true unless Rails.env.match(/production/)
+    redirect_to :protocol => "https://" unless request.ssl?
+    true
+  end
+
 end
