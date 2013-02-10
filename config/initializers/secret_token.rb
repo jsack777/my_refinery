43a67a5b709581c56d81c6cb3f323b87d76c3acb
Jeff Sackett <Jeff.Sackett@Pearson.com>
2013-02-09 18:36:33 -0600

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyCms::Application.config.secret_token = '5401d97439eabe039e7c3965a821bac452aab4b9d8be5ebb3a80d1dd3bbac1bcb24a0ac44328c89d1a328d324394f7d26e1282eeca51401369693b450e29e314'
